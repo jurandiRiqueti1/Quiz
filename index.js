8f961553ddd7b06835da0e8d1b6028619708d0df
Jurandi Riqueti Junior <jurandihd4000@gmail.con>
2022-09-18 01:20:46 -0300

    commit

--- index.js
+++ index.js
@@ -4,12 +4,62 @@ function questaoAleatoria(array) {
 
     const index = Math.floor(Math.random() * array.length)
 
-    const questao = array.slice(index, index+1)[0]
+    const questao = array.splice(index, 1)[0]
 
-    return {
-        questao: questao,
-        array: array
+    let questaoCorpo = document.querySelector("#questao-corpo")
+
+    if (questao != undefined) {
+        switch (questao.type) {
+            case 'escrever':
+                questaoCorpo.innerHTML = questao.pergunta
+                referencia(questao)
+                break;
+            case 'assinalar':
+                questaoCorpo.innerHTML = questao.pergunta
+                referencia(questao)
+                break;
+            case 'v-f':
+                questaoCorpo.innerHTML = questao.pergunta
+                referencia(questao)
+                break;
+        }
     }
 }
 
-console.log(questaoAleatoria(teste))
+function referencia(questao){
+    let divRef = document.querySelector("#questao-ref")
+    let aRef = document.querySelector("#link-ref")
+
+    if(Object.keys(questao).indexOf("ref") > -1){
+        divRef.classList.remove("d-none")
+        aRef.innerHTML = questao.ref
+        aRef.href = questao.ref
+    }else{
+        divRef.classList.add("d-none")
+    }
+}
+
+function clickInicio(event) {
+    event.preventDefault()
+
+    let divInicio = document.querySelector("#inicio")
+    let divQuestoes = document.querySelector("#questoes")
+
+    divInicio.classList.add("d-none")
+    divQuestoes.classList.remove("d-none")
+
+    
+    const formulario = document.querySelector("#formulario")
+    formulario.addEventListener('submit', clickQuestao)
+    questaoAleatoria(teste)
+}
+
+function clickQuestao(event) {
+    event.preventDefault()
+
+    questaoAleatoria(teste)
+}
+
+const inicio = document.querySelector("#form-inicio")
+
+inicio.addEventListener('submit', clickInicio)
